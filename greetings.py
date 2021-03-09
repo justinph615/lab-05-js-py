c4baa7887b0df2949853b2526e688d79770a7bc7
justinph615 <justinph615@gmail.com>
2021-03-08 19:04:58 -0500

    Lab 5 files

---
+++ greetings.py
@@ -0,0 +1,8 @@
+
+
+username = input("Hey what's your name? ")
+
+print("Hi " + str(username) + "!")
+
+
+	
---
+++ madlibs-user-input.py
@@ -0,0 +1,17 @@
+
+
+noun = input("Gimme a noun ")
+
+adjective = input("Gimme a adjective ") #uses user-submitted values
+
+adverb = input("Gimme a adverb ")
+
+verb = input("Gimme a verb ")
+
+print("On a tropical island " + str(noun) + ", ")
+print("Underneath a " + str(adjective) + " lava moon. ")
+print("Hangin'" + str(adverb) + " with the hulu dancers ")
+print(str(verb) + " questions because they got all the answers.")
+
+
+
